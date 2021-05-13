6862ce8d893b354ddffacc7931da045a9c88a2d0
Zhurkin_Alex <lexus1812141@gmail.com>
2021-05-13 10:27:02 +0300

    model student

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,22 @@
 import mongoose from "mongoose";
 
-const User = mongoose.model('User',{
+
+const Student = mongoose.model('Student',{
+    foto:{type:String},
     name:{type:String,trim:true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    steck: {type: String, required: true},
+    language: {type: String},
+    socialLinkedin: {type: String},
+    socialGitHab: {type: String},
+    placeWork:String,
+    vacancy:[{type:mongoose.Schema.Types.ObjectId , ref: "Vacansion"}],
+    commit:[{type:mongoose.Schema.Types.ObjectId , ref: "Commit"}],
+    resume:String,
+    fon: Number,
+    student:{type:Boolean, default:false},
+    admin:{type:Boolean, default:true},
 })
 
-export default User
+export default Student
