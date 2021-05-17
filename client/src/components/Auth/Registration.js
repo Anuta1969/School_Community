f708e05345d85a5ae01ef90b3182753d43aef3ad
Zhurkin_Alex <lexus1812141@gmail.com>
2021-05-17 20:34:02 +0300

    добавил стили

--- client/src/components/Auth/Registration.js
+++ client/src/components/Auth/Registration.js
@@ -15,6 +15,7 @@ function Registration(props) {
 
     return (
         <div className='registration'>
+          <div className="registr__form">
             <form onSubmit={(e) => registerHandler(e)} method='POST'
                   encType="multipart/form-data" className='formRegister'
             >
@@ -28,7 +29,8 @@ function Registration(props) {
                 <input className='addPhoto' type="file" name="photo" required/>
                 <button className='btnRegister' type='submit'>Регистрация</button>
             </form>
-            <video src={`${process.env.REACT_APP_URL}/`}></video>
+            </div>
+            
         </div>
     );
 }
