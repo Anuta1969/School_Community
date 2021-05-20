144e0f09c35495bcba0b7472a40de2134e483280
Zhurkin_Alex <lexus1812141@gmail.com>
2021-05-20 15:37:56 +0300

    изменения в организации

--- client/src/components/Сomment/Comment.js
+++ client/src/components/Сomment/Comment.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
+import './comment.css'
 
 function Comment({comment}) {
+  // console.log(comment.text);
+  const[textComment, setTextComment]= useState()
+  // const[commentObj]
+  const allcommentHandler =() =>{
+    const commentObj = comment.text.split('')
+    console.log(commentObj);
+    let commenObjText
+    let commentReturnText
+   
+    if(commentObj.length>50){
+      commenObjText = commentObj.splice(0,50)
+      commentReturnText = commenObjText.join("")
+      setTextComment(commentReturnText)
+    }
+    // console.log(commentObj);
+  }
+
+
   return (
-    <div>
-      <div> {comment.text} </div>
-      <div>Подробнее</div>
-      <div><a href={`/profile/${comment.author}`} >{comment.authorName}</a></div>
+    <div className="comment-title">
+      <div className="comment-item comment-item-text "> 
+      {/* {commentObj.length>50 && textComment} */}
+       </div>
+      <div className="comment-item" onClick={allcommentHandler}>Подробнее</div>
+      <div className="comment-item"><a href={`/profile/${comment.author}`} >{comment.authorName}</a></div>
     </div>
   );
 }
